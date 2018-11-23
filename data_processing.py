3b22d8816328f1f927a0d7c48c0dc3575e6da143
Brandon Martin-Anderson <badhill@gmail.com>
2018-11-23 15:37:27 -0800

    small var change

--- data_processing.py
+++ data_processing.py
@@ -340,15 +340,15 @@ def durations_to_distributions(df):
     df_stops_arr['key'] = 1
 
     #Get departure time hours for this dataset
-    df_timestamps = pd.DataFrame(df['departure_time_hour'].unique(), columns=['departure_time_hour'])
-    df_timestamps['key'] = 1
+    df_hour_dep = pd.DataFrame(df['departure_time_hour'].unique(), columns=['departure_time_hour'])
+    df_hour_dep['key'] = 1
 
     #Calculate create minutes array
     df_minutes = pd.DataFrame(np.arange(0,60), columns=['minute'])
     df_minutes['key'] = 1
 
     #Combine to form base time array
-    df_timestamps = df_timestamps.merge(df_minutes).merge(df_stops_dep).merge(df_stops_arr)
+    df_timestamps = df_hour_dep.merge(df_minutes).merge(df_stops_dep).merge(df_stops_arr)
     df_timestamps['departure_time_minute'] = df_timestamps['departure_time_hour'] + pd.to_timedelta(df_timestamps.minute, unit='m')
     df_timestamps = df_timestamps[['departure_stop_id', 'arrival_stop_id', 'departure_time_minute']]
     df_timestamps['departure_time_minute_unix'] = (df_timestamps['departure_time_minute'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
